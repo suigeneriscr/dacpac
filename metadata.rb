cc1642eb7a531aef419bb7581816b25e867e6ff9
Alfonso Aguilar <laltfuns@gmail.com>
2015-07-16 11:26:17 -0600

    Increasing version to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'luis.aguilar@suigeneris.com'
 license          'All rights reserved'
 description      'Deploys dacpac files'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 supports         'windows'
 depends          "windows", ">= 1.34.6"
